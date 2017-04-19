8a2d150cd82e0ea7b8e7452018003d350fc6aec8
Emrah Kocaman <emrahkocaman@gmail.com>
2017-04-19 11:30:32 +0300

    Initial commit

---
+++ src/main/java/com/hazelcast/test/Member.java
@@ -0,0 +1,12 @@
+package com.hazelcast.test;
+
+import com.hazelcast.core.Hazelcast;
+
+/**
+ * Created by emrah on 19/04/2017.
+ */
+public class Member {
+    public static void main(String[] args) {
+        Hazelcast.newHazelcastInstance();
+    }
+}
